e1f94d946ab12abb747b39f19760e34dd740ef3b
tkosushi <superluigi81@gmail.com>
2023-02-20 00:02:54 +0900

    modify data binding

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            // 検索結果の件数
-            count: 0,
             // セール対象のチェック(true: 有り、false: 無し)
             check1: false,
             // 送料無料のチェック(true: 有り、false: 無し)
@@ -18,6 +16,46 @@ const app = Vue.createApp({
                 {name: 'Ariel<br>スマホケース', price: 2580, image: 'images/05.jpg', shipping: 0, isSale: false},
                 {name: 'Azrael<br>スマホケース', price: 1280, image: 'images/06.jpg', shipping: 0, isSale: false}
             ]
+        }
+    },
+    computed: {
+        filteredList() {
+            // コンポーネントのインスタンスを取得
+            const app = this;
+            // 商品の絞り込み
+            const filteredList = this.list.filter(function(item){
+                // 表示判定 (true: 表示する、false: 表示しない)
+                let show = true;
+                // 検索条件：セール対象チェックあり
+                if (app.check1) {
+                    if (!item.isSale) {
+                        show = false;
+                    }
+                }
+                // 検索条件：送料無料チェックあり
+                if (app.check2) {
+                    if (item.shipping !== 0) {
+                        show = false;
+                    }
+                }
+                // 表示判定を返す
+                return show
+            });
+            // 商品の並べ替え
+            filteredList.sort(function(a,b){
+                // 「標準」が選択されている場合
+                if (app.order === 1) {
+                    // 元のlistと同じ順番なので何もしない
+                    return 0;
+                }
+                // 「安い順」が選択されている場合
+                else if (app.order === 2){
+                    // 価格が安い順にソート
+                    return a.price - b.price
+                }
+            });
+            // 商品リストを返す
+            return filteredList;
         }
     }
 })
